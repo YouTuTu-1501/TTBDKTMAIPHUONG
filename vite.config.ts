f44ffe6bafd6e3ae64231a494e20a6abbe1ae4a4
YouTuTu-1501 <youtu1501@gmail.com>
2026-07-09 23:22:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/TTBDKTMAIPHUONG/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
